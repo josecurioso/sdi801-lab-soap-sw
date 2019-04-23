4874a03e26058539d30003125b5c78b94ee22615
Jose Manuel Estrada-Nora Muñoz <jotajotanora@gmail.com>
2019-04-23 12:01:29 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ src/main/java/com/uniovi/soap/ws/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2019.04.23 a las 11:49:21 AM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/soap/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uniovi.soap.ws;
